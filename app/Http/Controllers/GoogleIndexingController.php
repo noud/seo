af94660d05ec986f4279a464f450031ebbc1f148
Noud de Brouwer <noud4@home.nl>
2019-12-26 17:06:15 +0100

    Google Indexing Service

--- app/Http/Controllers/GoogleIndexingController.php
+++ app/Http/Controllers/GoogleIndexingController.php
@@ -3,50 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Models\JobPosting;
-use Google;
-use Google_Service_Indexing_UrlNotification;
+use App\Services\Google\IndexingService;
 
 class GoogleIndexingController extends Controller
 {
-    private $indexing;
+    private $indexingService;
 
-    public function __construct()
+    public function __construct(IndexingService $indexingService)
     {
-        $this->indexing = Google::make('indexing');
+        $this->indexingService = $indexingService;
     }
 
     public function updateURL(JobPosting $jobPosting)
     {
-        $this->publish($jobPosting->thing->url, "URL_UPDATED");
+        $this->indexingService->publish($jobPosting->thing->url, "URL_UPDATED");
     }
 
     public function removeURL(JobPosting $jobPosting)
     {
-        $this->publish($jobPosting->thing->url, "URL_DELETED");
+        $this->indexingService->publish($jobPosting->thing->url, "URL_DELETED");
     }
 
     public function statusURL(JobPosting $jobPosting)
     {
-        $this->status($jobPosting->thing->url);
-    }
-
-    private function status(string $jobPostingURL)
-    {
-        $responce = $this->indexing->urlNotifications->getMetadata([
-            "url" => urlencode($jobPostingURL)
-        ]);
-
-        dd($responce);
-    }
-
-    private function publish(string $jobPostingURL, string $action)
-    {
-        $urlNotification = new Google_Service_Indexing_UrlNotification();
-        $urlNotification->setUrl($jobPostingURL);
-        $urlNotification->setType($action);
-
-        $responce = $this->indexing->urlNotifications->publish($urlNotification);
-
-        dd($responce);
+        $this->indexingService->status($jobPosting->thing->url);
     }
 }
