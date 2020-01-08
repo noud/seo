94d725907cdace8932de73999c734530365b5599
Noud de Brouwer <noud4@home.nl>
2020-01-08 23:28:10 +0100

    clean install

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use App\Models\Traits\Role;
 
+/**
+ * @todo vendor/reliese/laravel/src/Coders/Model/Model.php:684
+ */
 class Employee extends \App\Models\Base\Employee
 {
     use Role;
